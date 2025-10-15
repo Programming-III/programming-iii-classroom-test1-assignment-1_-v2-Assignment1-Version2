c08e7732aed5b9940f6d8a10b0075474f92a3144
Mariam Sadek <52850850+Mariamsadekk@users.noreply.github.com>
2025-10-15 15:47:45 +0300

    Update Course.h

--- Course.h
+++ Course.h
@@ -2,9 +2,41 @@
 #define COURSE_H
 
 #include "Student.h"
-
+#include <string>
 //define class course here
+class Course {
+private:
+std::string courseCode;
+std::string courseName;
+int maxStudents;
+Student** enrolledStudents; // dynamic array
+int currentCount;
+public:
+Course();
+Course(const std::string& code, const std::string& name, int maxStudents);
+~Course();
 
+
+// setters / getters
+void setCourseCode(const std::string& code);
+std::string getCourseCode() const;
+
+
+void setCourseName(const std::string& name);
+std::string getCourseName() const;
+
+
+void setMaxStudents(int m);
+int getMaxStudents() const;
+
+
+int getCurrentCount() const;
+
+
+// functionality
+bool addStudent(Student* s); // returns true if added
+void displayCourseInfo() const;
+};
 
 
 #endif
