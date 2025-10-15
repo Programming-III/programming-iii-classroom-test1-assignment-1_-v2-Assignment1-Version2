8ad9cd2cdef08c92628fa849379d068d6f9ea5aa
Mariam Sadek <52850850+Mariamsadekk@users.noreply.github.com>
2025-10-15 15:46:29 +0300

    Update Instructor.h

--- Instructor.h
+++ Instructor.h
@@ -4,5 +4,24 @@
 #include "Person.h"
 
 //define class instructor here
+class Instructor : public Person {
+private:
+std::string department;
+int experienceYears;
+public:
+Instructor();
+Instructor(const std::string& name, int id, const std::string& department, int experienceYears);
+~Instructor();
 
+
+void setDepartment(const std::string& d);
+std::string getDepartment() const;
+
+
+void setExperienceYears(int e);
+int getExperienceYears() const;
+
+
+void display() const override;
+};
 #endif
