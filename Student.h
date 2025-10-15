dc9f8428b724c380dbeaacd09b2de777e1daec4e
Mariam Sadek <52850850+Mariamsadekk@users.noreply.github.com>
2025-10-15 15:46:00 +0300

    Update Student.h

--- Student.h
+++ Student.h
@@ -3,7 +3,30 @@
 
 #include "Person.h"
 
-//write class Student here
+#include <string>
+
+
+class Student : public Person {
+private:
+int yearLevel;
+std::string major;
+public:
+Student();
+Student(const std::string& name, int id, int yearLevel, const std::string& major);
+~Student();
+
+
+// setters / getters
+void setYearLevel(int y);
+int getYearLevel() const;
+
+
+void setMajor(const std::string& m);
+std::string getMajor() const;
+
+
+void display() const override;
+};
 
 
 #endif
