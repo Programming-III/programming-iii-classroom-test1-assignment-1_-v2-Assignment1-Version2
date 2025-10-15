f542b07233aa270359a108da4ea0ad1529feed9d
Mariam Sadek <52850850+Mariamsadekk@users.noreply.github.com>
2025-10-15 15:44:43 +0300

    Update Person.h

--- Person.h
+++ Person.h
@@ -1,6 +1,32 @@
 #ifndef PERSON_H
 #define PERSON_H
 
-#write the class code here
+//write the class code here
+#include <string>
+#include <iostream>
 
+
+class Person {
+private:
+std::string name;
+int id;
+public:
+// constructors / destructor
+Person();
+Person(const std::string& name, int id);
+virtual ~Person();
+
+
+// getters / setters
+void setName(const std::string& name);
+std::string getName() const;
+
+
+void setId(int id);
+int getId() const;
+
+
+// display
+virtual void display() const;
+};
 #endif
