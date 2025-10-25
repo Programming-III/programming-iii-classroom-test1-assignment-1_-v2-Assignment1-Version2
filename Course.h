ebcf8eac4cf38765ea1042f3450202ea028b576e
Mariam Sadek <52850850+Mariamsadekk@users.noreply.github.com>
2025-10-25 17:06:17 +0300

    Update Course.h

--- Course.h
+++ Course.h
@@ -1,9 +1,39 @@
-#ifndef COURSE_H
-#define COURSE_H
 
-#include "Student.h"
-#include <string>
 //define class course here
 
+#ifndef COURSE_H
+#define COURSE_H
+#include "Student.h"
+#include "Person.h"
+#include <string>
 
-#endif
+class Course {
+private:
+    string courseCode;
+    string courseName;
+    int maxStudents;
+    Student** enrolledStudents; // dynamic array of Student*
+    int currentCount;
+
+public:
+    Course();
+    Course(const string& code, const string& name, int maxStudents);
+    ~Course();
+
+    void setCourseCode(const string& code);
+    string getCourseCode() const;
+
+    void setCourseName(const string& name);
+    string getCourseName() const;
+
+    void setMaxStudents(int m);
+    int getMaxStudents() const;
+
+    bool addStudent(Student* s); // returns true if added
+    void displayCourseInfo() const;
+
+    int getCurrentCount() const;
+};
+
+#endif // COURSE_H
+
